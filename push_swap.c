b836224b97f5787a6fa4ed92600b95eb1409894b
Mahmut Esat Yilmaz <mayilmaz@k1m23s07.42kocaeli.com.tr>
2025-01-19 18:26:49 +0300

    çok azkaldı

--- push_swap.c
+++ push_swap.c
@@ -94,7 +94,7 @@ int is_sorted(t_list **head_a) // (şu an kullanmıyorum) liste sortlanmışmı 
 	return(1);
 }
 
-int ft_optimize_move(t_list	**head, int	place) // eğer en optimize node stacin yarısından daha alttaysa 
+int ft_optimize_move(t_list	**head, int	place) // eğer en optimize node stacin yarısından daha alttaysa ra yerine rra yapıyor
 {
 	t_list	*node;
 	int size;
@@ -110,17 +110,11 @@ int ft_optimize_move(t_list	**head, int	place) // eğer en optimize node stacin 
 	return(place); 
 }
 
-int	ft_count_cost(t_list	**head_b, t_list	**head_a, int index_a, int	pla_a) // a stacindeki nodun maliyetini hesaplıyor
+int	ft_place_in_b(t_list	**head_b, int index_a, int	pla_b, int	len)
 {
-	t_list	*closest_smaller_node;
 	t_list	*top_b;
-	int		len;
-	int		pla_b;
-	int		cost;
+	t_list	*closest_smaller_node;
 
-	len = 0;
-	pla_b = 0;
-	cost = 0;
 	top_b = (*head_b);
 	closest_smaller_node = NULL;
 	while(top_b)
@@ -141,6 +135,19 @@ int	ft_count_cost(t_list	**head_b, t_list	**head_a, int index_a, int	pla_a) // a
 		top_b = top_b->next;
 		len++;
 	}
+	return(pla_b);
+}
+
+int	ft_count_cost(t_list	**head_b, t_list	**head_a, int index_a, int	pla_a) // a stacindeki nodun maliyetini hesaplıyor
+{
+	int		len;
+	int		pla_b;
+	int		cost;
+
+	len = 0;
+	pla_b = 0;
+	cost = 0;
+	pla_b = ft_place_in_b(head_b, index_a, pla_b, len);
 	pla_a = ft_optimize_move(head_a, pla_a);
 	pla_b = ft_optimize_move(head_b, pla_b);
 	cost = pla_a + pla_b;
@@ -182,14 +189,92 @@ void	ft_two_push(t_list	**head_a, t_list	**head_b) // a stacindeki ilk iki nodu 
 		ft_swap(head_b, 'b');
 }
 
-void	ft_sort(t_list	**head_a, t_list	**head_b) // ana sıralamayla ilgili fonksiyon
+void	ft_move(t_list	**head, int	cost_size, char op, char	stack)
+{
+	while (cost_size != 0)
+	{
+			if( stack == 'a')
+			{
+				if(op == 'r')
+					ft_reverse_rotate(head, stack);
+				else
+					ft_rotate(head, stack);
+			}
+			else if(stack == 'b')
+			{
+				if(op == 'r')
+					ft_reverse_rotate(head, stack);
+				else
+					ft_rotate(head, stack);
+			}
+			cost_size--;
+	}
+}
+
+void	ft_push_min(t_list	**head_a, t_list	**head_b, int	min_index)
+{
+	t_list *node_a;
+	int		cost_min;
+	int		new_cost_min;
+
+	cost_min = 0;
+	node_a = (*head_a);
+	while (node_a)
+	{
+		if(node_a->index == min_index)
+			break;
+		cost_min++;
+		node_a = node_a->next;
+	}
+	new_cost_min = ft_optimize_move(head_a, cost_min);
+	if(cost_min > new_cost_min)
+	{
+		cost_min = new_cost_min;
+		ft_move(head_a, cost_min, 'r', 'a');
+	}
+	else
+		ft_move(head_a, cost_min, 'd', 'a');
+	push_b(head_a, head_b);
+	printf("-------%d\n", min_index);
+}
+
+void	ft_make_head_in_b(t_list	**head_b, int index_a)
+{
+	int	len;
+	int	pla_b;
+	int new_pla_b;
+
+	len = 0;
+	pla_b = 0;
+	new_pla_b = 0;
+
+	pla_b = ft_place_in_b(head_b, index_a, pla_b, len);
+	new_pla_b = ft_optimize_move(head_b, pla_b);
+	if(pla_b > new_pla_b)
+	{
+		pla_b = new_pla_b;
+		ft_move(head_b, pla_b, 'r', 'b');
+	}
+	else
+		ft_move(head_b, pla_b, 'd', 'b');
+	//printf("-------%d\n", index_a);
+	
+}
+
+void	ft_sort_in_b(t_list	**head_a, t_list	**head_b) // ana sıralamayla ilgili fonksiyon
 {
 	t_list	*node;
 	t_list	*min_cost;
 
 	node = (*head_a);
 	ft_two_push(head_a, head_b);
-	min_cost = ft_less_cost(head_a, head_b);
+	while ((*head_a)->next->next->next)
+	{
+		min_cost = ft_less_cost(head_a, head_b);
+		//printf("min cost -------%d\n", min_cost->index);
+		ft_make_head_in_b(head_b, min_cost->index);
+		ft_push_min(head_a, head_b, min_cost->index);
+	}
 }
 
 void	ft_get_index(t_list **head_a) // nodeların sıralandıktan sonra olması gereken yerlerindeki indeksleri indeks değişkenine yazar
@@ -238,6 +323,8 @@ int main(int ac, char **av)
 		i++;
 	}
 	ft_get_index(&a);
-	ft_sort(&a, &b);
-	ft_print_list(&a);
+	ft_sort_in_b(&a, &b);
+	//ft_print_list(&a);
+	//printf("\n----------\n");
+	//ft_print_list(&b);
 }
